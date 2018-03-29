f48535ed6f7ac184b5d6d23b47f7fe59ce27a324
Erez Peer <erez.peer@gmail.com>
2018-03-29 14:29:59 +0000

    Impelemted CLI

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,8 +1,12 @@
 import argparse
 
+known_drivers = ['local', 's3']
+
 class DriverAction(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string=None):
 		driver, destination = values
+		if driver.lower() not in known_drivers:
+			parser.error("Unknown driver. Avilable drivers are 'local' & 's3'")
 		namespace.driver = driver.lower()
 		namespace.destination = destination
 
@@ -14,6 +18,7 @@ def create_parser():
 	parser.add_argument('--driver', 
 		help='how & where to store backup',
 		nargs=2,
+		metavar=("DRIVER", "DESTINATION"),
 		action=DriverAction,
 		required=True)
 	return parser
